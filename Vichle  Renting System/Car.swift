f5b1ef143579bcb19568b8be5ad78289ed337c46
C0772144 <komalsubhra6768@gmail.com>
2020-02-24 17:38:15 -0500

    variables declaration i

--- Vichle  Renting System/Car.swift
+++ Vichle  Renting System/Car.swift
@@ -38,64 +38,7 @@ class Car:Vehicle{
     var carColor: String
     var driver = [Int: Driver]()
     
-    init(vehicleIdentificationNumber :String,vehicleDiscription :String,manufacturerName :String,vehicleType:VehicleTypes,carColor:String,isSelfDrive : Bool,driverName:String,isInsured:Bool,insauranceProviderName : String?,noOfSeats: Int,fuelType: typesOfFuel,baseRatePerDay:Int,basePerKm:Int)
-    {
-        self.vehicleIdentificationNumber = vehicleIdentificationNumber
-        self.vehicleDiscription = vehicleDiscription
-        self.vehicleType = vehicleType
-        self.manufacturerName = manufacturerName
-        self.isSelfDrive = isSelfDrive
-        self.driverName = driverName
-        self.isInsured = isInsured
-        self.insauranceProviderName = insauranceProviderName
-        self.noOfSeats = noOfSeats
-        self.fuelType = fuelType
-        self.baseRatePerDay = baseRatePerDay
-        self.basePerKm = basePerKm
-       self.carColor = carColor
-    }
     
-    func addDriver(driverId: Int, driverObj: Driver)
-       {
-           driver.updateValue(driverObj, forKey: driverId)
-       }
-    func removeDriver(driverId: Int, driverObj: Driver)
-    {
-        driver.removeValue(forKey: driverId)
-    }
-       
-     
-    func display() {
-        print("_____________Car Details________________")
-        print("Vehicle Identification Number : \(self.vehicleIdentificationNumber)")
-        print("Vehicle Disctription : \(self.vehicleDiscription)")
-        print("Vehicle Type : \(self.vehicleType)")
-       
-        print("Car Color :\(self.carColor)")
-        print("Manufacturer Name :\(self.manufacturerName) ")
-        print("Is Self Drive :\(self.isSelfDrive)")
-        print("Driver Name :\(String(describing: self.driverName)) ")
-        print("Is Insured :\(self.isInsured)")
-        print("Insaurance Provider Name :\(String(describing: self.insauranceProviderName))")
-        print("No Of Seats :\(self.noOfSeats)")
-        print("Fuel Type :\(self.fuelType)")
-            print("Base Rate Per Day :\(self.baseRatePerDay.currency())")
-            print("Base Per KM :\(self.basePerKm.currency())" )
-        
-        if driver.count==0{
-            print("*******************************************************")
-            print("IT IS SELF DRIVE**NO DRIVER")
-            print("*******************************************************")
-        }
-        else{
-            print("*******************************************************")
-        for i in driver{
-            i.value.display()
-            }
-            
-        }
-        print("*******************************************************")
-    }
     
 
 }
