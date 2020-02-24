b931838e2ae978898fe9bb40dd2fc9241bf8e496
C0772144 <komalsubhra6768@gmail.com>
2020-02-24 18:25:32 -0500

    calling function display

--- Vichle  Renting System/Person/person.swift
+++ Vichle  Renting System/Person/person.swift
@@ -12,7 +12,20 @@ enum Gender{
     
 }
 protocol Person:DisplayDelegate{
+    var id:Int{get set}
+    var firstName:String{get set}
+    var lastName:String{get set}
+    var gender:Gender{get set}
+    var birthDate:String{get set}
+    var age : String?{get set}
+    var address:String { get set }
+    var city :String { get set }
+    var mobileNumber:Int{get set}
+    var emailId:String{get set}
+    var userName:String{get set}
+    var password:String{get set}
     
     
     
+    func display()
 }
