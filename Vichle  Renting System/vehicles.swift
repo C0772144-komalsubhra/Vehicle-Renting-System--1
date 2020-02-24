d8ebeb053f97d7cd583e50884215e2799612cb49
C0772144 <komalsubhra6768@gmail.com>
2020-02-24 17:27:33 -0500

    creating enum in vehicle types

--- Vichle  Renting System/vehicles.swift
+++ Vichle  Renting System/vehicles.swift
@@ -10,23 +10,9 @@ import Foundation
 enum  VehicleTypes {
     case BUS,CAR,MOTORCYCLE
 }
-protocol setvehicle:DisplayDelegate
-{
+
     
-    var vehicleIdentificationNumber :String { get set }
-    var vehicleDiscription :String { get set }
-    var manufacturerName :String { get set }
-    var isSelfDrive : Bool { get set }
-    var DriverName:String { get set }
-    var IsInsured:  Bool { get set }
-    var insauranceProviderName : String { get set }
-    var noOfSeats: Int { get set }
-    var fuelType: String { get set }
-    var baseRatePerDay:Int { get set }
-    var basePerKm:Int { get set }
-    var vehicletype:VehicleTypes{get set}
     
-    func display()
 }
 
     
